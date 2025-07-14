4a1fe1b43ba5f7012bca2b09d020f27c497ff108
koonkimb <koonkimb@gmail.com>
2025-07-14 13:32:00 -0400

    Fixed data scraper to ensure pages are iterated
    set more_pages back to True for next genre
    updated data from scraper

--- Final/myanimelist_scraper.py
+++ Final/myanimelist_scraper.py
@@ -19,21 +19,22 @@ anime_dict = {}
 
 def get_summaries(url):
 
-    driver.get(url)
+    driver.get(url) 
     soup = BeautifulSoup(driver.page_source, "html.parser")
 
-    summary = soup.find_all("div", {"class" : "synopsis js-synopsis"})
-    anime_id = soup.find_all("div", {"class" : "genres js-genre"}, id=True)
+    summary = soup.find_all("div", {"class" : "synopsis js-synopsis"}) # this is anime descriptions
+    anime_id = soup.find_all("div", {"class" : "genres js-genre"}, id=True) # this is anime id
     
     for i, j in zip(anime_id, summary):
-        anime_dict[i["id"]] = j.get_text().strip().replace('\n',' ').replace('\r', ' ')
+        anime_dict[i["id"]] = j.get_text().strip().replace('\n',' ').replace('\r', ' ').replace('\t', ' ') # anime id, descriptions in dict
 
 base_url = "https://myanimelist.net/anime/genre/"
-more_pages = True
+
 for a in range(83):
+    more_pages = True
     if a+1 in [9,49,12]: # not family friendly
         continue
-    driver = get_selenium()
+    driver = get_selenium() # new driver each time because of bot detection 
     genre_url = base_url + str(a+1)
     get_summaries(genre_url)
     time.sleep(2)
@@ -41,21 +42,21 @@ for a in range(83):
     screen_height = driver.execute_script("return window.screen.height;")
     driver.execute_script(f"window.scrollTo(0, {screen_height / 2});")
     while more_pages:
-        try:
-            driver.find_element(By.CSS_SELECTOR, "p.message")
+        try: # is current page 404?
+            driver.find_element(By.CSS_SELECTOR, "p.message") # 404 message - if it exists, then there are no more pages
             more_pages = False
-        except:
-            full_url = genre_url + "?page=" + str(page_index)
+        except: # if current page is not 404 then move onto next page
+            full_url = genre_url + "?page=" + str(page_index) # iterate to next page
             get_summaries(full_url)
             page_index += 1
             full_url = genre_url
-            time.sleep(2)
+            time.sleep(2) # website has bot detection
             screen_height = driver.execute_script("return window.screen.height;")
             driver.execute_script(f"window.scrollTo(0, {screen_height / 2});")
     full_url = base_url
     
-anime_dataframe = pd.DataFrame.from_dict(anime_dict, orient='index')
-anime_dataframe.to_csv("anime_summaries.csv",sep =';',index=True)
+anime_dataframe = pd.DataFrame.from_dict(anime_dict, orient='index') # dict to dataframe with index as keys
+anime_dataframe.to_csv("anime_summaries.csv",sep =';',index=True) # export to CSV
 
 
     
